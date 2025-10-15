08f4574e245289f5825caf010053ca60aeb5a498
princoo <princeineza@gmail.com>
2025-10-15 12:07:44 +0200

    chore-bn: add Prisma setup with User model and database migration

---
+++ backend/prisma/migrations/20251015100222_create_user_model/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "signature" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
